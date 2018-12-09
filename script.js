07c639e6efb6cb3dc98c6e3757d08f95dbd3790b
Imo-Hetherington <44382444+Imo-Hetherington@users.noreply.github.com>
2018-12-09 21:44:52 +0000

    jquery hover popups added

--- script.js
+++ script.js
@@ -1,13 +1,112 @@
-$(document).ready(function () {
-    $('#media/Prince-Charles-Cinema.jpg').hover(function () {
-        $(this).stop().animate({
-            opacity: .4
-        }, 200);
-        $('.text').removeClass('hide');
-    }, function () {
-        $(this).stop().animate({
-            opacity: 1
-        }, 500);
-        $('.text').addClass('hide');
-    });
+$(document).ready(function() {
+  $(".popuptext").hide();
+})
+
+$(document).ready(function() {
+$(".img1").mouseenter(function(){
+  $(".popup1").show();
 });
+})
+
+$(document).ready(function() {
+$(".img1").mouseleave(function(){
+  $(".popup1").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img2").mouseenter(function(){
+  $(".popup2").show();
+});
+})
+
+$(document).ready(function() {
+$(".img2").mouseleave(function(){
+  $(".popup2").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img3").mouseenter(function(){
+  $(".popup3").show();
+});
+})
+
+$(document).ready(function() {
+$(".img3").mouseleave(function(){
+  $(".popup3").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img4").mouseenter(function(){
+  $(".popup4").show();
+});
+})
+
+$(document).ready(function() {
+$(".img4").mouseleave(function(){
+  $(".popup4").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img5").mouseenter(function(){
+  $(".popup5").show();
+});
+})
+
+$(document).ready(function() {
+$(".img5").mouseleave(function(){
+  $(".popup5").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img6").mouseenter(function(){
+  $(".popup6").show();
+});
+})
+
+$(document).ready(function() {
+$(".img6").mouseleave(function(){
+  $(".popup6").hide();
+});
+})
+
+
+$(document).ready(function() {
+$(".img7").mouseenter(function(){
+  $(".popup7").show();
+});
+})
+
+$(document).ready(function() {
+$(".img7").mouseleave(function(){
+  $(".popup7").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img8").mouseenter(function(){
+  $(".popup8").show();
+});
+})
+
+$(document).ready(function() {
+$(".img8").mouseleave(function(){
+  $(".popup8").hide();
+});
+})
+
+$(document).ready(function() {
+$(".img9").mouseenter(function(){
+  $(".popup9").show();
+});
+})
+
+$(document).ready(function() {
+$(".img9").mouseleave(function(){
+  $(".popup9").hide();
+});
+})
