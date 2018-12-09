3a3fd80e13a5e7c1e1f1db6590837e7f09740ddb
Jessa <jessicaclaire94@googlemail.com>
2018-12-09 12:16:28 +0000

    closed hrefs, cleaned up formatting and added blurb for Prince Charles

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function expandInfo() {
   var popup = document.getElementById("#mainblurb");
   popup.classList.toggle("show")
   }
+
+  <div onmousemove="myMoveFunction()">
+    <p id="demo">I will demonstrate onmousemove!</p>
+  </div>
+
+  <div onmouseenter="myEnterFunction()">
+    <p id="demo2">I will demonstrate onmouseenter!</p>
+  </div>
+
+  <div onmouseover="myOverFunction()">
+    <p id="demo3">I will demonstrate onmouseover!</p>
+  </div>
